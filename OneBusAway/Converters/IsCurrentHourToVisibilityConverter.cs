aca850e71074f03bc399d6972cbfae02f8a2be54
Michael Braude <Michael.Braude@live.com>
2012-12-31 22:06:40 +0000

    Hilighting the current hour in the time table control.
    git-tfs-id: [https://mbraude.visualstudio.com:443/DefaultCollection]$/Windows8/OneBusAway;C126

---
+++ OneBusAway/Converters/IsCurrentHourToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace OneBusAway.Converters
+{
+    public class IsCurrentHourToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime[] valueTime = (DateTime[])value;
+            if (valueTime.Length == 0)
+            {
+                return 0;
+            }
+
+            return (valueTime[0].Hour == DateTime.Now.Hour)
+                ? parameter
+                : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
