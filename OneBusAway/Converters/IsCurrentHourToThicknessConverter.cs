460d45ffdcd2d58c129f47970acb660db0b2c27c
Michael Braude <Michael.Braude@live.com>
2013-01-04 20:27:50 +0000

    Misc. xaml fixes.
    git-tfs-id: [https://mbraude.visualstudio.com:443/DefaultCollection]$/Windows8/OneBusAway;C143

--- OneBusAway/Converters/IsCurrentHourToThicknessConverter.cs
+++ OneBusAway/Converters/IsCurrentHourToThicknessConverter.cs
@@ -8,19 +8,25 @@ using Windows.UI.Xaml.Data;
 
 namespace OneBusAway.Converters
 {
-    public class IsCurrentHourToVisibilityConverter : IValueConverter
+    public class IsCurrentHourToThicknessConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             DateTime[] valueTime = (DateTime[])value;
             if (valueTime.Length == 0)
             {
-                return 0;
+                return new Thickness(0);
+            }
+
+            double thickness = 0.0;
+            if (parameter != null)
+            {
+                double.TryParse(parameter as string, out thickness);
             }
 
             return (valueTime[0].Hour == DateTime.Now.Hour)
-                ? parameter
-                : 0;
+                ? new Thickness(thickness)
+                : new Thickness(0);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
