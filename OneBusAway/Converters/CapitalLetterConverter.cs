5490c5563b914c7f30daa65e094d0f1d77cbb59a
Michael Braude <Michael.Braude@live.com>
2013-01-11 08:19:41 +0000

    Adding a capital letter converter to help make sure all text is capitalized
    git-tfs-id: [https://mbraude.visualstudio.com:443/DefaultCollection]$/Windows8/OneBusAway;C157

--- OneBusAway/Converters/CapitalLetterConverter.cs
+++ OneBusAway/Converters/CapitalLetterConverter.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +7,19 @@ using Windows.UI.Xaml.Data;
 
 namespace OneBusAway.Converters
 {
-    /// <summary>
-    /// Allows in-line xaml string formats.
-    /// </summary>
-    public class StringFormatConverter : IValueConverter
+    public class CapitalLetterConverter : IValueConverter
     {
-        /// <summary>
-        /// Assumes the parameter is a string format.
-        /// </summary>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return (value == null)
-                ? string.Empty
-                : string.Format(CultureInfo.CurrentCulture, parameter.ToString(), value.ToString()).ToUpper();
+            string str = value as string;
+            if (!String.IsNullOrEmpty(str))
+            {
+                return str.ToUpper();
+            }
+
+            return String.Empty;
         }
-        
+
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotSupportedException();
