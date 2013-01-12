db10b25e3111e5c535e75ecedbddbfcd60f4cc64
Michael Braude <Michael.Braude@live.com>
2013-01-12 05:28:34 +0000

    Hide the map control toolbars when we're in snapped view.
    git-tfs-id: [https://mbraude.visualstudio.com:443/DefaultCollection]$/Windows8/OneBusAway;C160

---
+++ OneBusAway/Converters/BoolToInvertedConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace OneBusAway.Converters
+{
+    public class BoolToInvertedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool boolValue = (bool)value;
+            return !boolValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
