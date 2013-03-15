f779caf59a7b497d3b3080a451bded4b1de6966d
Michael Braude <Michael.Braude@live.com>
2013-03-15 06:23:45 +0000

    Changing the favorites star so that it is more prominent.
    git-tfs-id: [https://mbraude.visualstudio.com:443/DefaultCollection]$/Windows8/OneBusAway;C269

---
+++ OneBusAway/Controls/FavoriteControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace OneBusAway.Controls
+{
+    public sealed partial class FavoriteControl : UserControl
+    {
+        public FavoriteControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Fires the AddToFavorites command and passes it our data context.
+        /// </summary>
+        private void OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            NavigationController.Instance.AddToFavoritesCommand.Execute(this.DataContext);
+        }
+    }
+}
