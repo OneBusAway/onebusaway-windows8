d74100cdf5d322d3c7c710d3adfa3f3c0568cb33
Michael Braude <michael.braude@live.com>
2013-12-18 19:52:16 -0800

    Adding page control service and main page.

---
+++ OneBusAway.WindowsPhone/PageControls/PageControlService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneBusAway.PageControls;
+using OneBusAway.Services;
+
+namespace OneBusAway.WindowsPhone.PageControls
+{
+    public class PageControlService : IPageControlService
+    {
+        public PageControlService()
+        {
+        }
+
+        /// <summary>
+        /// Returns a page control for a given type.
+        /// </summary>
+        public IPageControl CreatePageControl(PageControlTypes pageControlType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OneBusAway.WindowsPhone/Pages/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using OneBusAway.PageControls;
+using OneBusAway.Pages;
+using OneBusAway.ViewModels.PageControls;
+
+namespace OneBusAway.WindowsPhone.Pages
+{
+    public partial class MainPage : PhoneApplicationPage, IMainPage
+    {
+        /// <summary>
+        /// Creates the main page.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Returns the current view model.
+        /// </summary>
+        public PageViewModelBase ViewModel
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Navigates to a page control.
+        /// </summary>
+        public void NavigateToPageControlByArguments(string arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets a page view.
+        /// </summary>
+        public void SetPageView(IPageControl pageControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Shows the help flyout.
+        /// </summary>
+        public void ShowHelpFlyout(bool calledFromSettings)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Shows the search pane.
+        /// </summary>
+        public void ShowSearchPane()
+        {
+        }        
+    }
+}
