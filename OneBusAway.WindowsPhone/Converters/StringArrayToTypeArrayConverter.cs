dec3cff55660ac0e8f2224c11d1f1d6b4dcda8cd
Michael Braude <michael.braude@live.com>
2014-02-01 23:36:15 -0800

    Fixing the time table control so that it properly handles weirdo Silverlight array of arrays...

---
+++ OneBusAway.WindowsPhone/Converters/StringArrayToTypeArrayConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace OneBusAway.Converters
+{
+    public class StringArrayToTypeArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string[] valueArray = ((string)value).Split(',');
+            Type convertToType = Type.GetType(parameter.ToString());
+            return valueArray.Select(val => System.Convert.ChangeType(val, convertToType)).ToArray();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
